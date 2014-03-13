d96f9cca7da3dc9afd50384c4e94db8f7c773114
Aaron Schumacher <ajschumacher@gmail.com>
2014-03-12 23:37:01 -0400

    nicer (?) output format

--- 02-R/monty_hall/aaron.R
+++ 02-R/monty_hall/aaron.R
@@ -1,7 +1,9 @@
 # R code outputting two values at the end
 
-print(1/3) # Probability of winning if not switching
-print(2/3) # Probability of winning if switching
+cat("Win probability if not switching:\n")
+print(1/3)
+cat("Win probability if switching:\n")
+print(2/3)
 
 # Sorry if I ruined it for you
 # Your script should output values determined by simulation
