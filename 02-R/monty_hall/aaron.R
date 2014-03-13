42a6e8c95acee8d1106ee0e47991624db66ee0a3
Aaron Schumacher <ajschumacher@gmail.com>
2014-03-12 23:14:08 -0400

    fill out structure for more R exercises

---
+++ 02-R/monty_hall/aaron.R
@@ -0,0 +1,7 @@
+# R code outputting two values at the end
+
+print(1/3) # Probability of winning if not switching
+print(2/3) # Probability of winning if switching
+
+# Sorry if I ruined it for you
+# Your script should output values determined by simulation
