aa5dfac465e119ade8ec243a74c0cf29490b83da
gimperion@gmail.com <Gimperion@gmail.com>
2014-03-14 06:27:13 -0400

    code cleanup

--- 02-R/monty_hall/tom.R
+++ 02-R/monty_hall/tom.R
@@ -7,15 +7,15 @@ items <- c('1'="goat", '2'="goat", '3'="car")
 
 monty_hall <- function(x){
     draw <- sample(items, 3) #normally bad form to call global from inside function but this is R, and re-instantiating an object, however small, is an even bigger sin.
-    
+    ## also we're sampling the entire set here to do a random sort and then picking the first element as our selection
     choice <- draw[1]
-    doors_left <- draw[2:3]
-    
-    door_opened <- sample(names(doors_left)[doors_left=='goat'],1)
-    door_remain <- doors_left[names(doors_left)!= door_opened]
+    doors_left <- sample(draw[2:3],2)
+
+    ## ie, since we sampled the remaining doors, we can assume that we're randomly picking door #1.  If door #1 is a car, we'll "display" door #2, then set door #1 to the result of the 1 door remaining.  If door #1 is a goat, we'll show door #1 and door #2 will be set to the result.
+    door_remain <- ifelse(doors_left[1]=='goat', doors_left[2], doors_left[1])
     
     swap_doors <- sample(0:1, 1)
-
+    ## boolean coersion 
     result <- ifelse(swap_doors, door_remain, choice)
     
     return(data.frame(iteration=x, switch_doors=swap_doors, win=ifelse(result=='car', 1, 0)))    
