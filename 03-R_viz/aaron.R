666cd07110c98d595450e65539020a94d4c801d0
Aaron Schumacher <ajschumacher@gmail.com>
2014-03-16 10:54:16 -0400

    starting point for optional hw

---
+++ 03-R_viz/aaron.R
@@ -0,0 +1,13 @@
+# define three functions
+
+# The arithmetic mean
+my_mean <- function(x) {}
+
+# Covariance
+my_cov <- function(x, y=x) {}
+
+# Pearson's correlation
+my_cor <- function(x, y) {}
+
+# test that they give the same results as mean, cov, cor
+# (at least for numeric vector arguments)
