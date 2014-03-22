ed1b8a22fe75007b09415b6efdebc5db82576e61
gimperion@gmail.com <Gimperion@gmail.com>
2014-03-21 20:26:24 -0400

    added comparison between feature scaling & plain KNN

--- 04-ML_KNN/tom.R
+++ 04-ML_KNN/tom.R
@@ -1,17 +1,27 @@
 ## 1NN on Iris 
 
-## Pull this out because I hate invisible data sets.
-mydata <- iris 
-
 ## Create a sample of 20 for testing set.
-test_index <- sample(seq_along(mydata$Species), 20, replace=FALSE)
-train <- mydata[-test_index,]
-test <- mydata[test_index,]
+normalize <- function(x) (x-mean(x))/sd(x)
+normalized_data <- iris
+normalized_data[,1:4] <- lapply(iris[,1:4], normalize)
+regular_data <- iris
 
-knn_one <- function(test_data, training_data) {
-    min_find <- apply(training_data[,1:4], 1, function(x,y) dist(rbind(x,y)), test_data)
-    return(training_data[which.min(min_find),'Species'])    
+RunKNN <- function(mydata){
+    test_index <- sample(seq_along(mydata$Species), 20, replace=FALSE)
+    train <- mydata[-test_index,]
+    test <- mydata[test_index,]
+
+    knn_one <- function(test_data, training_data) {
+        min_find <- apply(training_data[,1:4], 1, function(x,y) dist(rbind(x,y)), test_data)
+        return(training_data[which.min(min_find),'Species'])    
+    }
+
+    test$predicted <- apply(test[,1:4], 1, knn_one, train)
+    return(nrow(subset(test, Species==predicted)))
 }
 
-test$predicted <- apply(test[,1:4], 1, knn_one, train)
-cat("Success Rate: ", nrow(subset(test, Species==predicted)), " of 20.", fill=TRUE)
+norm_result <- replicate(100, RunKNN(normalized_data))
+reg_result <- replicate(100, RunKNN(regular_data))
+cat("Normalized Result: ", mean(norm_result), fill=TRUE)
+cat("Non-normed Result: ", mean(reg_result), fill=TRUE)
+
