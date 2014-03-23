8e7e3e705063c4c40ef8e0b1a4d32d12116517a0
gimperion@gmail.com <Gimperion@gmail.com>
2014-03-22 20:45:13 -0400

    use another method for working with double-apply

--- 04-ML_KNN/tom.R
+++ 04-ML_KNN/tom.R
@@ -6,7 +6,7 @@ normalized_data <- iris
 normalized_data[,1:4] <- lapply(iris[,1:4], normalize)
 regular_data <- iris
 
-RunKNN <- function(mydata){
+RunKNN_apply <- function(mydata){
     test_index <- sample(seq_along(mydata$Species), 20, replace=FALSE)
     train <- mydata[-test_index,]
     test <- mydata[test_index,]
@@ -20,8 +20,32 @@ RunKNN <- function(mydata){
     return(nrow(subset(test, Species==predicted)))
 }
 
+RunKNN_merge <- function(mydata){
+## This is how you would kinda do it in a database.
+    test_index <- sample(seq_along(mydata$Species), 20, replace=FALSE)
+    train <- mydata[-test_index,]
+    test <- mydata[test_index,]
+
+    knn_one <- function(test_data, training_data) {
+        test_data$key <- seq_along(test_data[,1])
+        big_df <- merge(training_data, test_data, by=NULL, all=TRUE)
+        big_df$distance <- apply(big_df[,c(1:4,6:9)], 1, function(x){
+            dist(rbind(c(x[1:4]), c(x[5:8])))
+        })
+        index <- aggregate(distance ~ key, data=big_df, which.min)
+        return(big_df$Species[index$distance])
+    }
+    test$predicted <- knn_one(test[,1:4], train)
+    return(nrow(subset(test, Species==predicted)))
+}
+
 norm_result <- replicate(100, RunKNN(normalized_data))
 reg_result <- replicate(100, RunKNN(regular_data))
 cat("Normalized Result: ", mean(norm_result), fill=TRUE)
 cat("Non-normed Result: ", mean(reg_result), fill=TRUE)
+
+system.time(replicate(20, RunKNN_apply(normalized_data)))
+system.time(replicate(20, RunKNN_merge(normalized_data)))
+## RunKNN_merge runs a lot slower in R.  Sadfase.
+
 
